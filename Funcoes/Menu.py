b44178efdf9f4d7d4036f4cf8259ebfcc12350f6
Felipe pinto de Brito <102550911+felipepinto16@users.noreply.github.com>
2023-02-05 18:10:43 -0300

    Atualizões

--- Funcoes/Menu.py
+++ Funcoes/Menu.py
@@ -1,4 +1,12 @@
 def menu():
+    from time import sleep
+    try:
+        arquivo = open("cadastrados.txt", "a")
+        print(f'Arquivo {arquivo} criado com sucesso !')
+        arquivo.close()
+    except:
+        print("Deu ruim !")
+
     while True:
         print('-=-'*20)
         print("MENU PRINCIPAL")
@@ -13,18 +21,41 @@ def menu():
                 break
             except:
                 print('\033[031mERROR: Por favor, digite um número inteiro válido')
-            
-        if deseja == 1 or deseja == 2 or deseja == 3:
-            if deseja == 1:
-                print('-=-'*20)
-                print('OPCÃO 1')
-                print('-=-'*20)
-                
-            elif deseja == 2:
-                print('-=-'*20)
-                print('OPÇÃO 2')
-                print('-=-'*20)
+        
+        if deseja == 1:
+            print('-=-'*20)
+            print('PESSOAS CADASTRADAS')
+            print('-=-'*20)
+            with open('cadastrados.txt', 'r') as cadastrados:
+                leitura = cadastrados.readlines()
+                for nomes in leitura:
+                    nomes = nomes.rstrip('\n')
+                    nome_idade = nomes.split(';')
 
-            print('\033[031mPor favor digite uma opção válida !')
+                    print(f'{nome_idade[0]:<60} {nome_idade[1]:<3} Anos.')
+
+        elif deseja == 2:
+            print('-=-'*20)
+            print('NOVO CADASTRO')
+            print('-=-'*20)
+            n  = input('Nome: ')
+            while True:
+                try:
+                    i = int(input('Idade: '))
+                    break
+                except ValueError:
+                    print('Idade e numero usuario :(')
+            with open('cadastrados.txt', 'a') as arquivo:
+                cadastrar = arquivo.write(f'\n{n}; {i}')
+            print(f'Novo registro !')
+
+        elif deseja == 3:
+            sleep(2)
+            print('Saindo do sistema... Até logo!')
+            break
+
+        else:
+            print('Digite uma opção válida !')
+    
 
 menu()
